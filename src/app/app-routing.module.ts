e277d8512fc1ea57883d1f42c91d871fb784656b
rhaneyko <rhaneykohonorio@gmail.com>
2025-03-18 22:01:46 -0400

    Configuração de rotas e módulo principal.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { PedidosComponent } from './features/pedidos/pedidos.component';
+import { MesasComponent } from './features/mesas/mesas.component';
+import { CardapioComponent } from './features/cardapio/cardapio.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'pedidos', component: PedidosComponent },
+  { path: 'mesas', component: MesasComponent },
+  { path: 'cardapio', component: CardapioComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { PedidosComponent } from './features/pedidos/pedidos.component';
+import { MesasComponent } from './features/mesas/mesas.component';
+import { CardapioComponent } from './features/cardapio/cardapio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    PedidosComponent,
+    MesasComponent,
+    CardapioComponent
   ],
   imports: [
     BrowserModule,
